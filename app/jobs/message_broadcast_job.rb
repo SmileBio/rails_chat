6211affe01aeeb67f4922fd10e95355843cc80a6
smile <smilebio@yandex.ru>
2017-07-09 19:30:58 +0300

    download fix bug

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(data)
-    Rails.env.production? ? "afternoon-ravine-81210.herokuapp.com" : "localhost"
+    Rails.env.production? ? host = "afternoon-ravine-81210.herokuapp.com" : host = "localhost"
     message = ""
     message = "<p>#{data.user.email}| #{(data.created_at).strftime('%Y-%m-%d %H:%M')} -> #{data.context}</p>" if data.context.length > 0
     message += "<p>#{data.user.email}| #{(data.created_at).strftime('%Y-%m-%d %H:%M')} -> <a download=#{host}/uploads/#{data.file_name} href=/uploads/#{data.file_name}>file</a></p>" if data.file_name
