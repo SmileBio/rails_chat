3c750c029fb2b14506dbbb7658679cd0bb1d60fa
smile <smilebio@yandex.ru>
2017-07-09 15:56:09 +0300

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis_onlines = Redis.new url: "redis://localhost:6379/0", db: 1
+$redis_onlines = Redis.new url: "redis://h:p13cc84dbc61209991252ec2a27a4cc9842d9031fdd36cc19dc9586c020fab499@ec2-34-230-117-175.compute-1.amazonaws.com:18759", db: 1
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 #Rails.application.config.session_store :cookie_store, key: '_go_friends_test_session'
-Rails.application.config.session_store :redis_store, servers: ["redis://localhost:6379/0/session"]
+Rails.application.config.session_store :redis_store, servers: ["redis://h:p13cc84dbc61209991252ec2a27a4cc9842d9031fdd36cc19dc9586c020fab499@ec2-34-230-117-175.compute-1.amazonaws.com:18759"]
